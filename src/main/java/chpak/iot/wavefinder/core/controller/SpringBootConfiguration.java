3bb8d20802a57ab02ec66a3b4651587314ab90cd
박세일(PARK SE IL)/Platform기술팀/SK <08158@skcc.com>
2016-06-29 19:04:44 +0900

    init commit

---
+++ src/main/java/chpak/iot/wavefinder/core/controller/SpringBootConfiguration.java
@@ -0,0 +1,12 @@
+package chpak.iot.wavefinder.core.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfiguration {
+
+	public static void main(String[] args) {
+
+	}
+
+}
