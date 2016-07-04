b8b8d0b2008fb25e52849e0288f3e78823e62452
heracul@naver.com <heracul@naver.com>
2016-07-04 21:15:48 +0900

    Service frame 작성

--- src/main/java/chpak/iot/wavefinder/core/controller/RelayController.java
+++ src/main/java/chpak/iot/wavefinder/core/controller/RelayController.java
@@ -1,17 +1,33 @@
 package chpak.iot.wavefinder.core.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import chpak.iot.wavefinder.core.service.MqttManagerService;
 
 @Controller
 @RequestMapping("/")
 public class RelayController {
 
+	@Autowired
+	MqttManagerService mqttManagerService;
+	
 	@RequestMapping
-	public @ResponseBody String callMqtt() {
+	public @ResponseBody String welcome() {
 		String returnTxt = "Hello Spring boot World";
 		return returnTxt;
+	}
+	
+	@RequestMapping("/tag/twitt/*")
+	public @ResponseBody ModelAndView requestFromTwitt(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mav = new ModelAndView();
+		return mav;
 	}
 	
 }
---
+++ src/main/java/chpak/iot/wavefinder/core/service/MqttManagerService.java
@@ -0,0 +1,8 @@
+package chpak.iot.wavefinder.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MqttManagerService {
+
+}
