de460639e0cf9ddaf7e6cf06542172fcec3839b5
heracul@naver.com <heracul@naver.com>
2016-07-05 20:31:39 +0900

    NETTY_PORT정의

--- src/main/java/chpak/iot/wavefinder/core/boot/SpringBootConfiguration.java
+++ src/main/java/chpak/iot/wavefinder/core/boot/SpringBootConfiguration.java
@@ -1,13 +1,15 @@
 package chpak.iot.wavefinder.core.boot;
 
 import io.moquette.server.Server;
+import io.moquette.server.config.IConfig;
 
 import java.io.IOException;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.StringUtils;
 
 @ComponentScan("chpak.iot.wavefinder.*")
 @EnableAutoConfiguration
@@ -18,7 +20,10 @@ public class SpringBootConfiguration {
 //		SpringApplication.run(SpringBootConfiguration.class, args); 
 		
 		final Server server = new Server();
-        server.startServer();
+		String nettyPort = System.getProperty("NETTY_PORT");
+		NettyConfig config = new NettyConfig();
+		config.setProperty("port", nettyPort);
+        server.startServer(config);
         System.out.println("Server started, version 0.9-SNAPSHOT");
         //Bind  a shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -33,4 +38,27 @@ public class SpringBootConfiguration {
 //		System.setProperty("moquette.path",  "config/moquette.conf"); //default path
 	}
 
+	
+}
+
+class NettyConfig implements IConfig {
+	private Map<String, String> configMap = new HashMap<String, String>();
+	@Override
+	public void setProperty(String name, String value) {
+		configMap.put(name, value);
+	}
+
+	@Override
+	public String getProperty(String name) {
+		return configMap.get(name);
+	}
+
+	@Override
+	public String getProperty(String name, String defaultValue) {
+		String value = getProperty(name);
+		if(StringUtils.isEmpty(value)) {
+			return defaultValue;
+		} 
+		return value;
+	}
 }
