956427da751d810f3c4f18cf60c9b840bdbab196
heracul@naver.com <heracul@naver.com>
2016-07-05 20:09:41 +0900

    모켓서버가 Main으로 설정되도록 변경함.

--- src/main/java/chpak/iot/wavefinder/core/boot/SpringBootConfiguration.java
+++ src/main/java/chpak/iot/wavefinder/core/boot/SpringBootConfiguration.java
@@ -1,5 +1,10 @@
 package chpak.iot.wavefinder.core.boot;
 
+import io.moquette.server.Server;
+
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +13,24 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class SpringBootConfiguration {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootConfiguration.class, args); 
+	public static void main(String[] args) throws IOException{
+		init();
+//		SpringApplication.run(SpringBootConfiguration.class, args); 
+		
+		final Server server = new Server();
+        server.startServer();
+        System.out.println("Server started, version 0.9-SNAPSHOT");
+        //Bind  a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                server.stopServer();
+            }
+        });
+	}
+	
+	private static void init() {
+//		System.setProperty("moquette.path",  "config/moquette.conf"); //default path
 	}
 
 }
