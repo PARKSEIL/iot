3ca88b6e170de99d875a5b5a0b5035b5229dc374
heracul@naver.com <heracul@naver.com>
2016-07-07 19:01:12 +0900

    MqttClientConstants 와 VO구성

---
+++ src/main/java/chpak/iot/wavefinder/core/constants/MqttClientConstants.java
@@ -0,0 +1,10 @@
+package chpak.iot.wavefinder.core.constants;
+
+public class MqttClientConstants {
+
+	public static final String TOPIC_KEY="topic";
+	public static final String MESSAGE_KEY="message";
+	public static final String BROKER_URL = "tcp://test.mosquitto.org:1883";
+	public static final String CLIENT_ID_KEY="clientId";
+	public static final String QOS_KEY = "qos";
+}
---
+++ src/main/java/chpak/iot/wavefinder/core/vo/MqttClientVO.java
@@ -0,0 +1,38 @@
+package chpak.iot.wavefinder.core.vo;
+
+public class MqttClientVO {
+
+	
+	private String clientId;
+	private String topic;
+	private String message;
+	private int qos;
+	
+	
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getTopic() {
+		return topic;
+	}
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getQos() {
+		return qos;
+	}
+	public void setQos(int qos) {
+		this.qos = qos;
+	}
+	
+	
+}
